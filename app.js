0e4cbda5d1077c3da0b382f95903d641d18cc201
Victor Benavides <Victor@Administrators-MacBook-Pro.local>
2023-11-28 23:23:31 -0500

    added input

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
 const fs = require('fs');
+const getNotes = require('./notes.js')
 const color = require('chalk');
 
-fs.writeFileSync('notes.txt', 'This file was created by node');
+// fs.writeFileSync('notes.txt', 'This file was created by node');
 
-fs.appendFileSync('notes.txt', 'Sup');
+// fs.appendFileSync('notes.txt', 'Sup');
 
-const validator = require('validator');
-console.log(validator.isEmail('email@example.com'));
-console.log(color.green('Success!'));
+// const validator = require('validator');
+// console.log(validator.isEmail('email@example.com'));
+// console.log(color.blue('Success!'));
+
+const command = process.argv[2];
+console.log(command);
+
+if(command === 'add'){
+    console.log('Adding Note!')
+}
+else if(command === 'remove'){
+    console.log('Removing Note!')
+}
